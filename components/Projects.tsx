3ebed7fc9eb229d33ce1bbd0596fc5fc0d9df580
Alhamdou <jallowalhamdou.9@gmail.com>
2023-08-19 09:54:33 +0000

    projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,68 +1,131 @@
-import Link from "next/link";
+import { motion } from "framer-motion";
 import React from "react";
-import { Cursor, useTypewriter } from "react-simple-typewriter";
-import BackgroundCircles from "./BackgroundCircles";
 import Image from "next/image";
 
-const imgURl =
-  "https://drive.google.com/file/d/1pZJodRWfmydS39R2QNcisah2QN644eVh/view?usp=drivesdk";
+type Props = {
+  data: Array<any>;
+};
 
-type Props = {data:any};
-
-export default function Hero({}: Props) {
-  const [text, count] = useTypewriter({
-    words: [
-      "Hey, Alhamdou Jallow here.",
-      "I build things for the web.",
-      "I Love doing Machine Learning and mobile Applicatio.",
-      "I love learning new technologies.",
-    ],
-    loop: true,
-    delaySpeed: 1000,
-  });
+export default function Projects({ data }: Props) {
+  const projects = [1, 2, 3, 4, 5];
   return (
-    <div className="pt-16 md:pt-28 h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
-      <BackgroundCircles />
-      <Image
-        width={200}
-        height={200}
-        className="relative h-32 w-32 rounded-full mx-auto object-cover"
-        src="/pic_mie.jpg"
-        alt="Alhamdou Jallow profile image"
-      />
-
-      <div className="z-20">
-        <h2 className="text-sm text-gray-400 uppercase tracking-[8px] md:tracking-[15px] pb-2 font-outfit">
-          Machine Learning Engineer and developer
-        </h2>
-        <h1 className="pt-2 text-3xl md:text-4xl lg:text-6xl font-semibold px-16">
-          <span className="mr-3 font-outfit">{text}</span>
-          <Cursor cursorColor="#aaaaaa" />
-        </h1>
-
-        <div className="pt-4 lg:pt-4 font-outfit">
-          <Link href="#about">
-            <button className="heroButton bg-slate-500/20 backdrop-blur-sm">
-              about
-            </button>
-          </Link>
-          <Link href={"#experience"}>
-            <button className="heroButton bg-slate-500/20 backdrop-blur-sm">
-              experience
-            </button>
-          </Link>
-          <Link href={"#skills"}>
-            <button className="heroButton bg-slate-500/20 backdrop-blur-sm">
-              skills
-            </button>
-          </Link>
-          <Link href={"#projects"}>
-            <button className="heroButton bg-slate-500/20 backdrop-blur-sm">
-              projects
-            </button>
-          </Link>
-        </div>
+    <div className="h-screen relative flex flex-col overflow-hidden text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+      <h3 className="absolute top-32 uppercase tracking-[20px] text-gray-400 text-2xl">
+        Projects
+      </h3>
+      <div className="absolute bottom-32 md:bottom-12 md:text-lg text-gray-200 z-40 items-center font-outfit animate-pulse opacity-75">
+        swipe to see more... <span className="text-blue-500">➡</span>
       </div>
+
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {data.map((project: any, i: any) => (
+          <motion.div
+            initial={{ opacity: 0.2 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            viewport={{ once: true }}
+            key={i}
+            className="w-screen flex-shrink-0 snap-center flex flex-col items-center justify-center p-10 md:p-44 h-screen mt-10 md:mt-28"
+          >
+            <Image
+              width={400}
+              height={400}
+              className="w-[300px] md:w-[400px] rounded-t-md object-contain"
+              src={project.imgurl}
+              alt=""
+            />
+
+            <div className="px-8 w-[300px] md:w-[400px] py-7 font-outfit bg-[#a9a9a9]/5 backdrop-blur-sm md:text-left rounded-b-md space-y-4 md:space-y-3">
+              <h4 className="text-xl font-semibold md:text-2xl">
+                {project.title}
+              </h4>
+
+              <div className="text-xs font-extralight space-x-4 mt-2 text-gray-200">
+                <span className="cursor-pointer underline">
+                  <a href={project.githubLink} target="_blank" rel="noreferrer">
+                    Github
+                  </a>
+
+                  <span className="inline-flex">
+                    <svg
+                      className="with-icon_icon__MHUeb"
+                      data-testid="geist-icon"
+                      fill="none"
+                      height="24"
+                      shapeRendering="geometricPrecision"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.5"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      style={{
+                        color: "currentColor",
+                        width: "14px",
+                        height: "14px",
+                      }}
+                    >
+                      <path d="M7 17L17 7"></path>
+                      <path d="M7 7h10v10"></path>
+                    </svg>
+                  </span>
+                </span>
+
+                <span className="cursor-pointer underline">
+                  <a
+                    href={project.deployedLink}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Live
+                  </a>
+                  <span className="inline-flex">
+                    <svg
+                      className="with-icon_icon__MHUeb"
+                      data-testid="geist-icon"
+                      fill="none"
+                      height="24"
+                      shapeRendering="geometricPrecision"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.5"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      style={{
+                        color: "currentColor",
+                        width: "14px",
+                        height: "14px",
+                      }}
+                    >
+                      <path d="M7 17L17 7"></path>
+                      <path d="M7 7h10v10"></path>
+                    </svg>
+                  </span>
+                </span>
+              </div>
+
+              <p className="text-sm font-light md:text-base mt-3 text-gray-100">
+                {project.content}
+              </p>
+
+              <div className="flex gap-2 mt-2 font-light text-sm items-center text-gray-400">
+                <span>tech stack used:</span>
+                {project.tech?.map((tech: any, i: any) => (
+                  <img
+                    key={i}
+                    src={tech.tech}
+                    alt=""
+                    className="w-5 h-5 rounded-sm object-contain"
+                  />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+        ))}
+      </div>
+
+      <div className="w-full absolute top-[27%] bg-gradient-to-r from-blue-500 opacity-30 left-0 h-[500px] -skew-y-12"></div>
     </div>
   );
 }
