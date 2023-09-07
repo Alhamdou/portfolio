b88e50a38ccbf5301e489c3c57b2a9f659b2d65b
Alhamdou <jallowalhamdou.9@gmail.com>
2023-09-07 17:28:46 +0000

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,8 @@ export default function Home({ data }: any) {
   return (
     <div id="mainBody" className="bg-black text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar-track-transparent scrollbar-thumb-slate-500/20 scrollbar-thin">
       <Head>
-        <title>Alhamdu</title>
-        <meta name="description" content="Alhamdou Jallow is a Machine Learning engineer and a software developer ready to tackle any challenge that is brought to him solving it using machine learning and any technologogy necessary" />
+        <title>Alhamdou Jallow</title>
+        <meta name="description" content="Alhamdou Jallow is a Machine Learning engineer and a software developer ready to tackle any challenge that is brought to him solving it using machine learning and any technologogy necessary. Alhamdou Jallow has work experience in various roles in the field of machine learning and software development. In 2018, they worked as a Software Developer at Gambia Youth Chamber Of Commerce. From January to July 2022, they served as a Machine Learning Engineer at Antern. In 2022, Alhamdou joined Omdena as a Junior Machine Learning Engineer, where they collaborated with individuals worldwide to utilize AI in solving societal issues in areas such as Agriculture, Health, and Retail. In 2023, they worked as a Machine Learning Intern at SAP" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico"/>
       </Head>
