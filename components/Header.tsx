9bd75aec6ff2e5675f79945ecdd5803af2cf9b9f
Alhamdou <jallowalhamdou.9@gmail.com>
2023-09-11 15:40:36 +0000

    updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -100,8 +100,8 @@ export default function Header({}: Props) {
           </svg>
         </p> */}
         <p className="hidden md:inline-flex text-sm text-[#a9a9a9]">
-          <a href="#contact">Get in touch</a>
-          +2202424045
+          <a href="#contact">Get in touch  +2202424045</a>
+          
         </p>
       </motion.div>
     </header>
